84d96a4acc204e6376e2dae47735f68776e351e0
Michael Di Giacomi <digitaladrenalin@gmail.com>
2018-08-31 09:45:28 -0400

    moved base url for server to environment.ts

--- src/app/pages/domainlist/domainlist.service.ts
+++ src/app/pages/domainlist/domainlist.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../../environments/environment';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class DomainlistService {
 
-  private url = 'http://home.hipsterconsulting.com:5000/api/domain';
+  private url = environment.VirshLibServer + 'api/domain';
 
   constructor(private http: HttpClient) {}
 
--- src/app/pages/sysinfo/sysinfo.service.ts
+++ src/app/pages/sysinfo/sysinfo.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SysinfoService {
 
-  private url = 'http://home.hipsterconsulting.com:5000/api/systeminfo';
+  private url = environment.VirshLibServer + 'api/systeminfo';
 
   constructor(private http: HttpClient) {}
 
